82f8ad5ed4b9a554a53873d49c02543aac1652ae
mohamed202100 <raafetmohamed0@gmail.com>
2025-09-12 18:21:00 +0300

    add posts page design

---
+++ resources/views/test.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+<a href="#" class="btn btn-success">Create Post</a>
+<table class="table mt-5 container">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Title</th>
+      <th scope="col">Posted By</th>
+      <th scope="col">Created At</th>
+      <th scope="col" colspan="3">ِActions</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($posts as $post)
+        <tr>
+            <th scope="row">{{$post['id']}}</th>
+            <td>{{$post['title']}}</td>
+            <td>{{$post['posted_by']}}</td>
+            <td>@{{$post['created_at']}}</td>
+            <td colspan="3">
+                <a href = "#" class="btn btn-info">View</a>
+                <a href = "#" class="btn btn-primary">Edit</a>
+                <a href = "#" class="btn btn-danger">Delete</a>
+            </td>
+        </tr>
+    @endforeach
+
+  </tbody>
+</table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
